a564d9da489ef7fefe07e0ff53f0427f774f5935
WindBlack <2474429669@qq.com>
2023-11-11 21:49:23 +0800

    用户-修改地址

--- src/main/java/com/soft2242/shop/service/UserShippingAddressService.java
+++ src/main/java/com/soft2242/shop/service/UserShippingAddressService.java
@@ -2,10 +2,13 @@ package com.soft2242.shop.service;
 
 import com.soft2242.shop.entity.UserShippingAddress;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.soft2242.shop.vo.AddressVO;
+
+import java.util.List;
 
 /**
  * <p>
- *  服务类
+ * 服务类
  * </p>
  *
  * @author ycshang
@@ -13,4 +16,26 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface UserShippingAddressService extends IService<UserShippingAddress> {
 
+    /**
+     * 添加收获地址
+     * 
+     * @param addressVO
+     * @return
+     */
+    Integer saveShippingAddress(AddressVO addressVO);
+
+    /**
+     * 修改收货地址
+     * 
+     * @param addressVO
+     * @return
+     */
+    Integer editShippingAddress(AddressVO addressVO);
+
+    /**
+     * 获取收获地址列表
+     *
+     * @return
+     */
+    List<AddressVO> userShippingAddressList(Integer userId);
 }
--- src/main/java/com/soft2242/shop/service/impl/UserShippingAddressServiceImpl.java
+++ src/main/java/com/soft2242/shop/service/impl/UserShippingAddressServiceImpl.java
@@ -1,20 +1,92 @@
 package com.soft2242.shop.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.soft2242.shop.common.exception.ServerException;
+import com.soft2242.shop.convert.AddressConvert;
 import com.soft2242.shop.entity.UserShippingAddress;
+import com.soft2242.shop.enums.AddressDefaultEnum;
 import com.soft2242.shop.mapper.UserShippingAddressMapper;
 import com.soft2242.shop.service.UserShippingAddressService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.soft2242.shop.vo.AddressVO;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author ycshang
  * @since 2023-11-07
  */
 @Service
-public class UserShippingAddressServiceImpl extends ServiceImpl<UserShippingAddressMapper, UserShippingAddress> implements UserShippingAddressService {
+public class UserShippingAddressServiceImpl extends ServiceImpl<UserShippingAddressMapper, UserShippingAddress>
+    implements UserShippingAddressService {
+
+    /**
+     * 添加收获地址
+     *
+     * @param addressVO
+     * @return
+     */
+    @Override
+    public Integer saveShippingAddress(AddressVO addressVO) {
+        UserShippingAddress convert = AddressConvert.INSTANCE.convert(addressVO);
+        if (addressVO.getIsDefault() == AddressDefaultEnum.DEFAULT_ADDRESS.getValue()) {
+            List<UserShippingAddress> list = baseMapper.selectList(new LambdaQueryWrapper<UserShippingAddress>()
+                .eq(UserShippingAddress::getIsDefault, AddressDefaultEnum.DEFAULT_ADDRESS.getValue()));
+            if (list.size() > 0) {
+                throw new ServerException("已存在默认地址，请勿重复操作");
+            }
+        }
+        save(convert);
+        return convert.getId();
+    }
+
+    /**
+     * 修改收货地址
+     *
+     * @param addressVO
+     * @return
+     */
+    @Override
+    public Integer editShippingAddress(AddressVO addressVO) {
+        UserShippingAddress userShippingAddress = baseMapper.selectById(addressVO.getId());
+        if (userShippingAddress == null) {
+            throw new ServerException("地址不存在");
+        }
+        if (addressVO.getIsDefault() == AddressDefaultEnum.DEFAULT_ADDRESS.getValue()) {
+            UserShippingAddress address = baseMapper.selectOne(
+                new LambdaQueryWrapper<UserShippingAddress>().eq(UserShippingAddress::getUserId, addressVO.getUserId())
+                    .eq(UserShippingAddress::getIsDefault, AddressDefaultEnum.DEFAULT_ADDRESS.getValue()));
+            if (address != null) {
+                address.setIsDefault((AddressDefaultEnum.NOT_DEFAULT_ADDRESS.getValue()));
+                updateById(address);
+            }
+        }
+        UserShippingAddress address = AddressConvert.INSTANCE.convert(addressVO);
+        updateById(address);
+        return address.getId();
+    }
+
+    /**
+     * 获取收获地址列表
+     *
+     * @param userId
+     * @return
+     */
+    @Override
+    public List<AddressVO> userShippingAddressList(Integer userId) {
+        UserShippingAddress userShippingAddress = baseMapper.selectById(userId);
+        if(userShippingAddress == null){
+            throw new ServerException("用户不存在");
+        }
+        List<UserShippingAddress> list = baseMapper.selectList(new LambdaQueryWrapper<UserShippingAddress>()
+            .eq(UserShippingAddress::getUserId, userShippingAddress.getUserId()));
+        List<AddressVO> addressVOList = AddressConvert.INSTANCE.convertToAddressVOList(list);
+        return addressVOList;
+    }
 
 }
