442f5506f3c087d778ac1525e78072af8531be8c
WindBlack <2474429669@qq.com>
2023-11-25 08:47:20 +0800

    提交订单

--- src/main/java/com/soft2242/shop/service/UserOrderService.java
+++ src/main/java/com/soft2242/shop/service/UserOrderService.java
@@ -2,15 +2,27 @@ package com.soft2242.shop.service;
 
 import com.soft2242.shop.entity.UserOrder;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.soft2242.shop.entity.UserOrderGoods;
+import com.soft2242.shop.vo.UserOrderVO;
+
+import java.util.List;
 
 /**
  * <p>
- *  服务类
+ * 服务类
  * </p>
  *
  * @author ycshang
  * @since 2023-11-07
  */
 public interface UserOrderService extends IService<UserOrder> {
+
+    /**
+     * 提交订单
+     * 
+     * @param orderVO
+     * @return
+     */
+    Integer addGoodsOrder(UserOrderVO orderVO);
 
 }
--- src/main/java/com/soft2242/shop/service/impl/UserOrderGoodsServiceImpl.java
+++ src/main/java/com/soft2242/shop/service/impl/UserOrderGoodsServiceImpl.java
@@ -6,6 +6,8 @@ import com.soft2242.shop.service.UserOrderGoodsService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
  * <p>
  *  服务实现类
@@ -17,4 +19,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserOrderGoodsServiceImpl extends ServiceImpl<UserOrderGoodsMapper, UserOrderGoods> implements UserOrderGoodsService {
 
+    /**
+     * 批量插入订单记录
+     *
+     * @param list
+     */
+    @Override
+    public void batchUserOrderGoods(List<UserOrderGoods> list) {
+        saveBatch(list);
+    }
 }
